b225392b6f8ceed3b5bdee81bff23537e46294e0
Dung201103 <21t1020322@husc.edu.vn>
2025-08-23 11:05:56 +0700

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,28 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducer/rootReducer';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { persistStore, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 const persistConfig = {
-    key: 'root',
-    storage,
-}
+  key: 'root',
+  storage,
+};
 
-const persistedReducer = persistReducer(persistConfig, rootReducer)
-const store = createStore(persistedReducer, composeWithDevTools(applyMiddleware(thunk)))
-let persistor = persistStore(store)
-export { store, persistor }
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// chỉ enable devtools khi NODE_ENV === 'development'
+const composeEnhancers =
+  process.env.NODE_ENV !== 'production'
+    ? composeWithDevTools
+    : compose;
+
+const store = createStore(
+  persistedReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
+
+let persistor = persistStore(store);
+
+export { store, persistor };
